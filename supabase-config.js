5644f6c5e5a08c927116ddf7ec45c596758200a6
ALIF ARYA DZAKII <aliifaryatiktok@gmail.com>
2026-07-31 17:31:15 +0700

    Update Supabase configuration URL and anonKey

--- supabase-config.js
+++ supabase-config.js
@@ -2,8 +2,8 @@
 // SUPABASE CONFIG - Single Source of Truth
 // ============================================================
   const SUPABASE_CONFIG = {
-  url: 'https://tiloslwfdfepcofmtros.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpbG9zbHdmZGZlcGNvZm10cm9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0NDI2NjYsImV4cCI6MjEwMTAxODY2Nn0.oCySVoTjzcLb7EtYs9cLntNdZLCIxoq2IiAR5gILJ_w'
+  url: 'https://fcprtymqtdhfafprdkze.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjcHJ0eW1xdGRoZmFmcHJka3plIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0ODg0NDAsImV4cCI6MjEwMTA2NDQ0MH0.g54qhjxZ7Ce9qdk_WDKeDy4SqxlSc4IZHHfy3uW8Rg8'
 };
 
 // Ekspor untuk digunakan di file lain
