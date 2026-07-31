4a87ada54d513e14d80b884a88b1335fdcd5767d
ALIF ARYA DZAKII <aliifaryatiktok@gmail.com>
2026-07-31 07:35:16 +0700

    Update Supabase configuration with new URL and key

--- supabase-config.js
+++ supabase-config.js
@@ -2,8 +2,8 @@
 // SUPABASE CONFIG - Single Source of Truth
 // ============================================================
 const SUPABASE_CONFIG = {
-  url: 'https://meclpcxdlyybjbmhfxru.supabase.co',
-  anonKey: 'sb_publishable_GJl4nv43kFDxc_PzAo90qg_zbXMmTiT'
+  url: 'https://tiloslwfdfepcofmtros.supabase.co',
+  anonKey: 'sb_publishable_jwyhKRP2UMZzFm9lECA-eA_tN362lyl'
 };
 
 // Ekspor untuk digunakan di file lain
