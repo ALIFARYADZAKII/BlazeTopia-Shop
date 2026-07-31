1f61165e5eb545d4745391ab6733c8d3e4390b6c
ALIF ARYA DZAKII <aliifaryatiktok@gmail.com>
2026-07-31 08:06:11 +0700

    Change anonKey in supabase-config.js
    Updated anonKey in the SUPABASE_CONFIG object.

--- supabase-config.js
+++ supabase-config.js
@@ -1,9 +1,9 @@
 // ============================================================
 // SUPABASE CONFIG - Single Source of Truth
 // ============================================================
-const SUPABASE_CONFIG = {
+  const SUPABASE_CONFIG = {
   url: 'https://tiloslwfdfepcofmtros.supabase.co',
-  anonKey: 'sb_publishable_jwyhKRP2UMZzFm9lECA-eA_tN362lyl'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpbG9zbHdmZGZlcGNvZm10cm9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0NDI2NjYsImV4cCI6MjEwMTAxODY2Nn0.oCySVoTjzcLb7EtYs9cLntNdZLCIxoq2IiAR5gILJ_w'
 };
 
 // Ekspor untuk digunakan di file lain
